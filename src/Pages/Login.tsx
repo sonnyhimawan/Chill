68f2580489820b0b1500958ffd5aaba53279c9ca
Sonny <Sonnyhimawan13456@gmail.com>
2025-06-09 21:17:42 +0700

    Fixing login screen dan register

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -3,10 +3,10 @@ import LoginForm from '../components/organism/FormLogin';
 const Login = () => {
   return (
     <div
-      className="h-screen bg-cover bg-center flex items-center justify-center"
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
       style={{ backgroundImage: "url('assets/img/bg-login.jpg')" }}
     >
-      <div className="bg-background/80 px-8 py-4 rounded-lg w-full max-w-md m-4">
+      <div className="bg-background/80 px-6 py-4 rounded-lg w-full max-w-md m-4">
         <LoginForm />
       </div>
     </div>
--- src/Pages/Register.tsx
+++ src/Pages/Register.tsx
@@ -3,10 +3,10 @@ import RegisterForm from '../components/organism/FormRegister';
 const Register = () => {
   return (
     <div
-      className="h-screen bg-cover bg-center flex items-center justify-center"
+      className="min-h-screen bg-cover bg-center flex items-center justify-center"
       style={{ backgroundImage: "url('assets/img/bg-register.jpg')" }}
     >
-      <div className="bg-background/80 px-8 py-4 rounded-lg w-full max-w-md m-4">
+      <div className="bg-background/80 px-6 py-4 rounded-lg w-full max-w-md m-4">
                 <RegisterForm />
         </div>
 
